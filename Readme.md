1da63ac427250f71f1773a5e38c26b0050e977f3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to visualize pivot grid data via the XtraCharts Suite
+
+
+<p>The following example shows how to bind a Chart Control to a Pivot Grid to visualize data.</p><p>In this example, after an ASPxPivotGrid and Chart Control are created, the pivot grid's ID is assigned to the WebChartControl.DataSourceID property. By default, the WebChartControl.AutoBindingSettingsEnabled property is set to true, so that binding settings are automatically adjusted, and no further customization is needed.  Once the Chart Control is bound to the ASPxPivotGrid, pivot grid data is immediately displayed in the Chart.</p><p>The sample web page contains the 'Transpose Data Source' check box, which defines the pivot grid's PivotGridOptionsChartDataSource.ProvideDataByColumns property value. If the check box is checked, the property is set to false, and series are created based on pivot grid rows (instead of columns, which is default behavior).</p>
+
+<br/>
+
+
---
+++ VB/ASPxPivotGrid_ChartIntegration/Default.aspx.vb
@@ -0,0 +1,27 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web.UI
+
+Namespace ASPxPivotGrid_ChartIntegration
+	Partial Public Class _Default
+		Inherits Page
+		Protected Overrides Sub OnInit(ByVal e As EventArgs)
+			MyBase.OnInit(e)
+
+			' Specifies that Series data member values are created based on columns.
+			ASPxPivotGrid1.OptionsChartDataSource.ProvideDataByColumns = True
+
+			' Specifies that data binding settings are automatically adjusted 
+			' while retrieving data from the pivot grid.
+			WebChartControl1.AutoBindingSettingsEnabled = True
+
+			' Sets the pivot grid as the chart's data source.
+			WebChartControl1.DataSourceID = "ASPxPivotGrid1"
+		End Sub
+		Protected Sub ASPxCheckBox1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxPivotGrid1.OptionsChartDataSource.ProvideDataByColumns = Not ASPxCheckBox1.Checked
+		End Sub
+	End Class
+End Namespace
